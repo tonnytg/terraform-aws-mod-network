000e1ee6c15c0d6a75afcea93088c20a72b72d07
Antonio Thomacelli <tonnytg@gmail.com>
2021-01-30 13:00:08 -0300

    rename output without aws_

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
-output "aws_vpc_id" {
+output "vpc_id" {
   value = aws_vpc.this.id
 }
 
-output "aws_vpc_cidr_block" {
+output "vpc_cidr_block" {
   value = aws_vpc.this.cidr_block
 }
 
-output "aws_internet_gateway_id" {
+output "internet_gateway_id" {
   value = aws_internet_gateway.this.id
 }
 
-output "aws_eip_id" {
+output "eip_id" {
   value = aws_eip.this.id
 }
 
-output "aws_eip_public_ip" {
+output "eip_public_ip" {
   value = aws_eip.this.public_ip
 }
